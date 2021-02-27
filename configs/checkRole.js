aa79ed40811c6ca3804c9683b0cb05b7c87d0965
Jayant Dhawan <jayantdhawan95@gmail.com>
2021-02-27 19:11:33 +0530

    fixed always returning NOT_AUTHORIZED

--- configs/checkRole.js
+++ configs/checkRole.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = function (req, res, next) {
   try {
     if (req.headers.authorization && req.headers.authorization.split(' ')[0] === "Bearer") {
-      const user = jwt.verify(req.headers.authorization.split(' ')[0], process.env.JWT_SECRET);
+      const user = jwt.verify(req.headers.authorization.split(' ')[1], process.env.JWT_SECRET);
       if (user.role === "admin") {
         next();
       } else {
